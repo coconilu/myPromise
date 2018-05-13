043fb69e072b0fb1f3f2627491aba5c6894a8a11
Marksim <827130441@qq.com>
2018-05-13 22:33:01 +0800

    用立即调用函数做测试函数

--- test/PromiseTester.js
+++ test/PromiseTester.js
@@ -1,9 +1,78 @@
-const { MyPromise } = require('../src/Promise')
+const {
+    MyPromise
+} = require('../src/Promise');
 
-MyPromise.resolve()
-.then(value=>{console.log(value)},err=>{console.log(err)})
-.then(value=>{console.log(value);throw new Error('test')},err=>{console.log(err)})
-.then(value=>{console.log(value)},err=>{console.log(err)})
-.catch(err=>{console.log(err)})
-.then(value=>{console.log(value)},err=>{console.log(err)})
-.finally(()=>{console.log('end')})
+// 基本测试
+(() => {
+    MyPromise.resolve()
+        .then(value => {
+            console.log(value)
+        }, err => {
+            console.log(err)
+        })
+        .then(value => {
+            console.log(value);
+            throw new Error('test')
+        }, err => {
+            console.log(err)
+        })
+        .then(value => {
+            console.log(value)
+        }, err => {
+            console.log(err)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        .then(value => {
+            console.log(value)
+        }, err => {
+            console.log(err)
+        })
+        .finally(() => {
+            console.log('end')
+        })
+});
+
+
+// race测试
+(() => {
+    var myRace = MyPromise.race([1, MyPromise.resolve(2).then(value => {
+        return new MyPromise(resolve => {
+            setTimeout(() => {
+                resolve(value)
+            }, 3000)
+        })
+    }), MyPromise.resolve(3).then(value => {
+        return new MyPromise(resolve => {
+            setTimeout(() => {
+                resolve(value)
+            }, 2000)
+        })
+    })])
+
+    myRace.then(value => {
+        console.log(value)
+    })
+});
+
+// all测试
+(() => {
+    var myAll = MyPromise.all([1, MyPromise.resolve(2).then(value => {
+        return new MyPromise(resolve => {
+            setTimeout(() => {
+                resolve(value)
+            }, 3000)
+        })
+    }), MyPromise.resolve(3).then(value => {
+        return new MyPromise(resolve => {
+            setTimeout(() => {
+                resolve(value)
+            }, 2000)
+        })
+    })])
+
+    myAll.then(value => {
+        console.log(value)
+    })
+})();
