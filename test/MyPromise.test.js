17817bcb3a9bf224ca99b1f8608cc3b38bda7ff6
alpha <827130441@qq.com>
2018-10-03 17:43:47 +0800

    using Jest test my code

--- test/MyPromise.test.js
+++ test/MyPromise.test.js
@@ -1,42 +1,23 @@
-const {
-    MyPromise
-} = require('../src/MyPromise');
+const { MyPromise } = require('../src/MyPromise');
 
 // 基本测试
-let baseTest = () => {
+test('基本测试', done => {
     MyPromise.resolve('start')
         .then(value => {
-            console.log(value)
-            throw new Error('error1')
-        }, err => {
-            console.log(err)
-        })
-        .then(value => {
-            console.log(value);
-        }, err => {
-            console.log(err)
-            throw new Error('error2')
+            expect(value).toBe('start')
+            throw new Error('error')
         })
         .catch(err => {
-            console.log(err)
+            expect(err.message).toBe('error')
         })
         .finally(() => {
-            console.log('end')
+            done()
         })
-}
-
-// 测试报错
-let errorTest = () => {
-    new MyPromise(() => {
-        throw new Error('test')
-    }).catch(err => {
-        console.log(err)
-    })
-}
+})
 
 // race测试
-let raceTest = () => {
-    var myRace = MyPromise.race([1, MyPromise.resolve(2).then(value => {
+test('race测试', done => {
+    let myRace = MyPromise.race([1, MyPromise.resolve(2).then(value => {
         return new MyPromise(resolve => {
             setTimeout(() => {
                 resolve(value)
@@ -51,13 +32,14 @@ let raceTest = () => {
     })])
 
     myRace.then(value => {
-        console.log(value)
+        expect(value).toBe(1)
+        done()
     })
-}
+})
 
 // all测试
-let allTest = () => {
-    var myAll = MyPromise.all([1, MyPromise.resolve(2).then(value => {
+test('all测试', done => {
+    let myAll = MyPromise.all([1, MyPromise.resolve(2).then(value => {
         return new MyPromise(resolve => {
             setTimeout(() => {
                 resolve(value)
@@ -72,11 +54,9 @@ let allTest = () => {
     })])
 
     myAll.then(value => {
-        console.log(value)
+        expect(value[0]).toBe(1)
+        expect(value[1]).toBe(2)
+        expect(value[2]).toBe(3)
+        done()
     })
-}
-
-baseTest()
-// errorTest()
-// allTest()
-// raceTest()
+})
